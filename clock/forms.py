c0cbba02c938be6e12d865a152aca93afeb726a4
Jason McMullen <jjm35205@live.com>
2016-11-07 17:27:39 -0500

    First commit
    Basic model and form logic completed.

---
+++ clock/forms.py
@@ -0,0 +1,50 @@
+"""
+forms.py
+
+@author Jason McMullen
+"""
+
+from clock.models import Employee, Punch
+from django import forms
+from django.contrib.auth.models import User
+from django.forms import ModelForm
+from django.core.exceptions import ObjectDoesNotExist
+from django.core.validators import RegexValidator
+
+class RegistrationForm(ModelForm):
+
+    username = forms.CharField(label='User Name')
+    email = forms.EmailField(label='E-Mail Address')
+    password = forms.CharField(label='Password')
+    password = forms.CharField(label='Confirm Password', widget=forms.PasswordInput(render_value=False))
+
+    # Pull in all information from Employee class except for username
+    class Meta:
+
+        model = Employee
+        exclude = ('user',)
+
+    # This method checks to see if the username is available for use
+    def clean_username(self):
+
+        username = self.cleaned_data['username']
+        try:
+            User.objects.get(username=username)
+        except User.DoesNotExist:
+            return username
+        raise forms.ValidationError("That user name already exists. Please select another.")
+
+    # This method checks to see if the passwords equal each other
+    def clean_password1(self):
+
+        if self.cleaned_data.get('password') != self.cleaned_data.get('password1'):
+             raise forms.ValidationError("The passwords did not match. Please try again.")
+        return self.cleaned_data
+
+"""
+Information needed to be gathered for LoginRequest
+"""
+class LoginForm(forms.Form):
+
+    username = forms.CharField(label='User Name')
+    password = forms.CharField(label='Password', widget=forms.PasswordInput(render_value=False))
---
+++ clock/models.py
@@ -0,0 +1,41 @@
+"""
+models.py
+
+@author Jason McMullen
+"""
+
+from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import RegexValidator
+
+# An extension of the User class    
+class Employee(models.Model):
+
+    # Validator to control what the user is entering into the fields
+    alpha = RegexValidator(r'^[a-zA-Z]*$', 'Only alpha characters without spaces are allowed.')
+
+    user = models.OneToOneField(User)
+    first_name = models.CharField(max_length=20, null=False, blank=False, validators=[alpha])
+    last_name = models.CharField(max_length=20, null=False, blank=False, validators=[alpha])
+    phone_number = models.CharField(max_length=20, null=False, blank=False)
+    pay_rate = models.FloatField(default=0.00, null=False, blank=True)
+    can_see_hours = models.BooleanField(default=False, null=False, blank=True)
+
+AUTH_USER_MODEL = 'clock.Employee'
+
+# Logic to handle all punch ins/outs. For now, accept all punch requests
+class Punch(models.Model):
+
+    date = models.DateField()
+    time = models.TimeField()
+    employee = models.ForeignKey('Employee')
+
+    def __str__(self):
+
+        # Format for time
+        formatT = "%H:%M:%S"
+
+        # "American" date format
+        formatD = "%a %b %d %Y"
+
+        return "Punch at " + " " + self.date.strftime(formatD) + " " +self.time.strftime(formatT)
